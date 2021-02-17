ad6010975713063f3d0675934641fe55b9dcbad3
alimuamar11 <alimuamar11@gmail.com>
2021-02-17 19:23:18 +0700

    views

---
+++ app/Views/pages/about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aboutme</title>
+</head>
+
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, obcaecati odio omnis rem necessitatibus eveniet eum eius modi adipisci alias quo debitis in ducimus id, ipsam commodi quos repellendus excepturi.</p>
+</body>
+
+</html>
